c9f6765563a13c77c9c975ab58bbde2ccbd6d604
Gabriel Lima <gabriellfsouza@gmail.com>
2020-01-17 07:38:34 -0300

    chore: project structure and application theme updated (to do app)

---
+++ frontend/src/pages/ToDoApp/index.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+import React, { memo } from "react";
+import ReactDOM from "react-dom";
+
+import { useInputValue, useTodos } from "./custom-hooks";
+
+import Layout from "../components/Layout";
+
+import AddTodo from "../components/AddTodo";
+import TodoList from "../components/TodoList";
+
+export default TodoApp = memo(props => {
+  const { inputValue, changeInput, clearInput, keyInput } = useInputValue();
+  const { todos, addTodo, checkTodo, removeTodo } = useTodos();
+
+  const clearInputAndAddTodo = _ => {
+    clearInput();
+    addTodo(inputValue);
+  };
+
+  return (
+    <Layout>
+      <AddTodo
+        inputValue={inputValue}
+        onInputChange={changeInput}
+        onButtonClick={clearInputAndAddTodo}
+        onInputKeyPress={event => keyInput(event, clearInputAndAddTodo)}
+      />
+      <TodoList
+        items={todos}
+        onItemCheck={idx => checkTodo(idx)}
+        onItemRemove={idx => removeTodo(idx)}
+      />
+    </Layout>
+  );
+});
